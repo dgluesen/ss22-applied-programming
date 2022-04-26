6d762c7efc25ff383efb3f04cdfcaf88dc03e8e3
Dennis Gluesenkamp <data@gluesenkamp.info>
2022-04-26 16:04:29 +0200

    Update after lecture 04

--- 04_python2/examplemodule.py
+++ 04_python2/examplemodule.py
@@ -10,4 +10,7 @@ coord4 = [7, 5, 7]
 
 # Function to calculate euclidean distance metric
 def calcEuclideanDist(c1, c2):
+    """
+    Function calculates euclidean distance of two points in 3D space
+    """
     return math.sqrt(pow(c1[0]-c2[0], 2) + pow(c1[1]-c2[1], 2) + pow(c1[2]-c2[2], 2))
